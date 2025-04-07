cb269ecabc9b2eae2c71ea3aa2af6c9a7d65a6ec
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2025-04-06 18:08:24 -0700

    Add lightningcss for css-modules
    index.css remains non module since it is for global general styles (i.e. body). Remove some unecessary styles from template

---
+++ src/component/App.jsx
@@ -0,0 +1,12 @@
+import appCSS from '../style/App.module.css'
+
+function App() {
+  return (
+    <div className={appCSS.app}>
+      <h1>Home</h1>
+      <h2>yep</h2>
+    </div>
+  )
+}
+
+export default App
