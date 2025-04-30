9c5cb8708d5e21b703b596116e2fdf1fdf0d56bd
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2025-04-30 01:04:46 -0700

    Add vitest and other testing packages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test/setup.js',
+  },
   css: {
     transformer: "lightningcss",
     lightningcss: {
